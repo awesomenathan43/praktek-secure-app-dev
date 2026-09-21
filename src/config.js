012236afd57f5ea2ac301ad8e0c5842c74c1c78e
awesomenathan43 <nathanaelsuhardi@gmail.com>
2026-09-21 16:29:57 +0700

    semua

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
   port: process.env.PORT || 3000,
 
   // Secret untuk menandatangani JWT
-  jwtSecret: process.env.jwkSecret,
+  jwtSecret: process.env.example.jwkSecret,
 
   // API key payment gateway (palsu)
   paymentGatewayApiKey: process.env.paymentGetwayApiKey,
